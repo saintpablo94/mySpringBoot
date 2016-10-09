272274dc2d90007bc6d53423870f3ed299e70d46
daesung kim <daesung kim>
2016-10-09 20:29:10 +0900

    third commit

---
+++ src/main/java/readinglist/SecurityConfig.java
@@ -0,0 +1,16 @@
+package readinglist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private ReaderRepository readerRepository;
+	
+	
+}
